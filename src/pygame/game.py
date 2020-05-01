3cc9d96dcb70bbd82fbce6fac0ebbaf870d2817f
arynchoong <arynchoong@hotmail.com>
2020-05-01 11:33:15 +0800

    Removed STATE from pygame basics tutorial

--- src/pygame/game.py
+++ src/pygame/game.py
@@ -3,13 +3,9 @@ import pygame
 # Dimensions
 WIDTH = 640
 HEIGHT = 480
-# States
-START = 1
-PLAYING = 2
-GAMEOVER = 3
 #            R    G    B
 BLACK   = (  0,   0,   0)
-GREEN   = (  0, 255,   0)
+GREEN   = (  3, 192,  60)
 
 class Game():
     def __init__(self):
@@ -18,7 +14,6 @@ class Game():
         self.display = pygame.display.set_mode(self.size)
         self.disp_surf = self.init_display()
         self.running = True if self.disp_surf else False
-        self.state = START
         self.font = pygame.font.Font(pygame.font.get_default_font(), 36)
         self.ball = pygame.image.load("ball.png")
         self.ballrect = self.ball.get_rect()
@@ -34,43 +29,25 @@ class Game():
         # Event loop
         while self.running:
             self.check_events()
-            if self.state == PLAYING:
-                self.render_play()
-            else:
-                self.render()
+            self.render()
         return
 
     def render(self):
         '''Render surface'''
+        # background
         self.disp_surf.fill(BLACK)
-
-        # set the text for display
-        if self.state == START:
-            text = 'PyGame Basics'
-            message = 'Press space to play.'
-        else:
-            text = 'Game Over'
-            message = 'State: %s' % self.state
-        # draw the messages
-        self.draw_text(text, GREEN, (int(WIDTH / 2), int(HEIGHT / 2) - 50))
-        self.draw_text(message, GREEN, (int(WIDTH / 2), int(HEIGHT / 2) + 50))
-      
+        # draw text
+        text = 'PyGame Basics'
+        self.draw_text(text, GREEN, (int(WIDTH / 2), int(HEIGHT / 2)))
         # Blit everything to screen
         self.display.blit(self.disp_surf, (0,0))
-        pygame.display.flip()
-        return
 
-    def render_play(self):
-        '''Render surface'''
         # draw ball
         self.ballrect = self.ballrect.move(self.speed)
         if self.ballrect.left < 0 or self.ballrect.right > WIDTH:
             self.speed[0] = -self.speed[0]
         if self.ballrect.top < 0 or self.ballrect.bottom > HEIGHT:
-            self.speed[1] = -self.speed[1]
-        
-        # fill background and draw ball
-        self.display.fill(BLACK)
+            self.speed[1] = -self.speed[1]        
         self.display.blit(self.ball, self.ballrect)
 
         # update display
@@ -81,14 +58,7 @@ class Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-                self.state = GAMEOVER
                 return
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    if self.state == GAMEOVER:
-                        self.state = START
-                    else:
-                        self.state +=1
         return
     
     def draw_text(self, text, colour, center):
