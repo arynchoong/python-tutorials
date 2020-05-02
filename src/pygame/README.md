bba250567031325c56f491d227c2e0252dc17270
arynchoong <arynchoong@hotmail.com>
2020-05-02 10:41:47 +0800

    Extension of pygame basics.

--- src/pygame/README.md
+++ src/pygame/README.md
@@ -4,3 +4,8 @@ Simple Game to introduce PyGame basics
 
 # References
  * [Introduction to Pygame](https://www.pygame.org/docs/tut/PygameIntro.html)
+ * [https://www.pygame.org/docs/tut/chimp.py.html](https://www.pygame.org/docs/tut/ChimpLineByLine.html)
+
+## Sound files
+ * [thump.wav](https://freesound.org/people/newagesoup/sounds/369120/) by newagesoup
+ * [swish.wav](https://freesound.org/people/www.bonson.ca/sounds/12658/) by www.bonson.ca
--- src/pygame/game.py
+++ src/pygame/game.py
@@ -15,12 +15,16 @@ class Game():
         self.disp_surf = self.init_display()
         self.running = True if self.disp_surf else False
         self.font = pygame.font.Font(pygame.font.get_default_font(), 36)
-        self.ball = pygame.image.load("ball.png")
-        self.ballrect = self.ball.get_rect()
-        self.speed = [2, 2]
+        self.ball = Ball()
+        self.hand = Hand()
+        self.allsprites = pygame.sprite.RenderPlain((self.ball, self.hand))
+        self.sound_hit = pygame.mixer.Sound("thump.wav")
+        self.sound_missed = pygame.mixer.Sound("swish.wav")
+        self.score = 0
     
     def init_display(self):
         pygame.display.set_caption('PyGame Basics Workshop')
+        pygame.mouse.set_visible(0)
         # Create surface
         disp_surf = pygame.Surface(self.size)
         return disp_surf.convert()
@@ -37,18 +41,14 @@ class Game():
         # background
         self.disp_surf.fill(BLACK)
         # draw text
-        text = 'PyGame Basics'
+        text = "Spin the Ball : %s" % self.score
         self.draw_text(text, GREEN, (int(WIDTH / 2), int(HEIGHT / 2)))
         # Blit everything to screen
         self.display.blit(self.disp_surf, (0,0))
 
         # draw ball
-        self.ballrect = self.ballrect.move(self.speed)
-        if self.ballrect.left < 0 or self.ballrect.right > WIDTH:
-            self.speed[0] = -self.speed[0]
-        if self.ballrect.top < 0 or self.ballrect.bottom > HEIGHT:
-            self.speed[1] = -self.speed[1]        
-        self.display.blit(self.ball, self.ballrect)
+        self.allsprites.update()
+        self.allsprites.draw(self.display)
 
         # update display
         pygame.display.flip()
@@ -58,7 +58,19 @@ class Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-                return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.hand.hit(self.ball):
+                    self.ball.hit()
+                    self.sound_hit.play()
+                    self.score += 1
+                    if self.score % 10 == 0:
+                        self.ball.level_up()
+                else:
+                    self.sound_missed.play()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.hand.unhit()
         return
     
     def draw_text(self, text, colour, center):
@@ -67,6 +79,84 @@ class Game():
         text_rect = text_surf.get_rect()
         text_rect.center = center
         self.disp_surf.blit(text_surf, text_rect)
+
+
+# classes for our game objects
+
+class Ball(pygame.sprite.Sprite):
+    """Bounce a ball around the screen. It can spin when it is hit."""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)  # call Sprite intializer
+        self.screen = pygame.display.get_surface()
+        self.image = pygame.image.load("ball.png")
+        self.rect = self.image.get_rect()
+        self.speed = [2, 2]
+        self.spin = 0
+
+    def update(self):
+        """move or spin, depending on the spin state"""
+        if self.spin:
+            self._spin()
+        else:
+            self._move()
+
+    def _move(self):
+        """move the ball across the screen, reverse speed at the ends"""
+        self.rect = self.rect.move(self.speed)
+        if self.rect.left < 0 or self.rect.right > WIDTH:
+            self.speed[0] = -self.speed[0]
+        if self.rect.top < 0 or self.rect.bottom > HEIGHT:
+            self.speed[1] = -self.speed[1]
+
+    def _spin(self):
+        """spin the ball image"""
+        center = self.rect.center
+        self.spin += 60
+        if self.spin >= 360:
+            self.spin = 0
+            self.image = self.original
+        else:
+            rotate = pygame.transform.rotate
+            self.image = rotate(self.original, self.spin)
+        self.rect = self.image.get_rect(center=center)
+
+    def hit(self):
+        """this will cause the ball to start spinning"""
+        if not self.spin:
+            self.spin = 60
+            self.original = self.image
+            # Reverse direction
+            self.speed = [-x for x in self.speed]
+
+    def level_up(self):
+        self.speed = [2*x for x in self.speed]
+
+
+class Hand(pygame.sprite.Sprite):
+    """moves a hand icon on the screen, following the mouse"""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
+        self.image = pygame.image.load("hand.png")
+        self.rect = self.image.get_rect()
+        self.hitting = 0
+
+    def update(self):
+        """move the fist based on the mouse position"""
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+        if self.hitting:
+            self.rect.move_ip(5, 10)
+
+    def hit(self, target):
+        """returns true if the hand collides with the target"""
+        if not self.hitting:
+            self.hitting = 1
+            hitbox = self.rect.inflate(-5, -5)
+            return hitbox.colliderect(target.rect)
+
+    def unhit(self):
+        """called to pull the hand back"""
+        self.hitting = 0
 
 
 if __name__ == "__main__":
